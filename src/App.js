d526604c640ffe9427a13cb57c3b653588cc18c4
Saaim-K <saaimkhan221@gmail.com>
2022-11-01 02:39:45 +0500

    Learning-CRUD

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import db from './firebase-config'
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+
+    const getUsers = async () => {
+
+    }
+  }, [])
+  getUsers()
+
+
   return (
-   <>
-   
-   </>
+    <>
+
+    </>
   );
 }
 
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -17,4 +17,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+export const db = getFirestore(app);
