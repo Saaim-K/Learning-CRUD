60e17e942d30dfe54f531357bec89e46b1342b9e
Saaim-K <saaimkhan221@gmail.com>
2022-11-02 01:56:20 +0500

    Learning-CRUD

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import { collection, getDocs } from 'firebase/firestore';
 
 function App() {
   const [users, setUsers] = useState([]);
-  const userCollectionRef = collection(db, "users")//from what collection you want data from?
+  const userCollectionRef = collection(db, "users")//from which collection you want data from?
   useEffect(() => {
 
     const getUsers = async () => {
       const data = await getDocs(userCollectionRef);
       console.log(data);
       setUsers(data.docs.map(()=>{
-        
+
       }))
     };
     getUsers()
