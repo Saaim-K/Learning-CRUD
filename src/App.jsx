7c81b08fc2a20fa988c1b6f534f52c735418f040
Saaim-K <saaimkhan221@gmail.com>
2022-11-07 03:04:24 +0500

    Learnig-CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import './App.css';
 import { db } from './firebase-config'
+import moment from 'moment/moment'; 
 import { useEffect, useState } from 'react';
-import { collection, doc, getDocs, addDoc, deleteDoc } from 'firebase/firestore';
+import { collection, doc, getDocs, addDoc, deleteDoc, onSnapshot, query } from 'firebase/firestore';
 
 function App() {
   const [users, setUsers] = useState([]);
   const [newName, setNewName] = useState('');
   const [newAge, setNewAge] = useState(0);
 
-
-  // -------------------- From which Collection you want data from --------------------
+  // ---------------------------------------- From which Collection you want data from ----------------------------------------
   const userCollectionRef = collection(db, "users")//from which collection you want data from?
-  // -------------------- From which Collection you want data from --------------------
+  // ---------------------------------------- From which Collection you want data from ----------------------------------------
 
 
+  // ---------------------------------------- UseEffect Hook ----------------------------------------
 
-  // ---------------------------------------- Getting Data From Firebase ----------------------------------------
   useEffect(() => {
+
+    // ---------------------------------------- Getting Data From Firebase ----------------------------------------
     const getUserData = async () => {
       const data = await getDocs(userCollectionRef);
       console.log("Data Docs", data.docs);
@@ -25,18 +27,42 @@ function App() {
       setUsers(data.docs.map((getData) => ({ ...getData.data(), id: getData.id })));
       console.log("Users", users);
       // -------------------- Populating Users State with Data from Database --------------------
-      
+
     };
-    getUserData()
+    // getUserData()
+    // ---------------------------------------- Getting Data From Firebase ----------------------------------------
+
+
+    // ---------------------------------------- Getting Real Time Data From Firebase ----------------------------------------
+    let unsubscribe;
+    const realTimeData = () => {
+      const q = query(userCollectionRef);
+      unsubscribe = onSnapshot(q, realData => {
+        // -------------------- Populating Users State with Data from Database --------------------
+        setUsers(realData.docs.map((getData) => ({ ...getData.data(), id: getData.id })))
+        // -------------------- Populating Users State with Data from Database --------------------
+      });
+    }
+    realTimeData();
+    // ---------------------------------------- Getting Real Time Data From Firebase ----------------------------------------
+
+
+    // The useEffect Hook is built in a way that we can return a function inside it and this return function is where the cleanup happens.The cleanup function prevents memory leaks and removes some unnecessary and unwanted behaviors.
+    return () => {
+      console.log('Cleanup Function');
+      unsubscribe();
+    }
+
 
   }, [])
-  // ---------------------------------------- Getting Data From Firebase ----------------------------------------
+
+  // ---------------------------------------- UseEffect Hook ----------------------------------------
 
 
   // ---------------------------------------- Create User ----------------------------------------
   const addUser = async () => {
     await addDoc(userCollectionRef,
-      { 
+      {
         name: newName,
         age: Number(newAge),
         createdOn: new Date().getTime()
@@ -76,9 +102,9 @@ function App() {
       {/* ---------------------------------------- Displayed Div ---------------------------------------- */}
       {users.map((users, i) => (
         <div key={i}>
-          <h1>Name : {users.name}</h1>
-          <h1>Age : {users.age} </h1>
-          <h4>Date : {users.createdOn} </h4>
+          <h3>Name : {users.name}</h3>
+          <h3>Age : {users.age} </h3>
+          <h4>Date : {moment(users.createdOn).fromNow('ss')} </h4>
           <h4>ID : {users.id} </h4>
           <button onClick={updateAge}>UPDATE</button>
           <button onClick={() => { deleteUser(users.id); }}>DELETE</button>
