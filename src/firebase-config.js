f1eeace6a59013112005d20828b91f0ecf7af06f
Saaim-K <saaimkhan221@gmail.com>
2022-11-06 11:35:52 +0500

    Learning-CRUD

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,17 +3,19 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API,//AIzaSyB-Xjtj0V5D6aRlTLYBQfKjSN2rRUffABo
+    // apiKey: "AIzaSyB-Xjtj0V5D6aRlTLYBQfKjSN2rRUffABo",
+    apiKey: process.env.REACT_APP_FIREBASE_API,
     authDomain: "learning-crud-e1d32.firebaseapp.com",
     projectId: "learning-crud-e1d32",
     storageBucket: "learning-crud-e1d32.appspot.com",
     messagingSenderId: "931183424307",
     appId: "1:931183424307:web:13cac3775bf4bd91c2c446",
     measurementId: "G-J3QSLNM8G9"
-}
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
